20190d4ffd93cf248aa4a2368adcab8bc2606773
Guna1301 <gskotipalli2005@gmail.com>
2025-03-01 23:48:53 +0530

    added play button component

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import FeaturedGridSkeleton from "@/components/skeletons/FeaturedGridSkeleton"
 import { useMusicStore } from "@/stores/useMusicStore"
+import PlayButton from "./PlayButton"
 
 function FeaturedSection() {
     const {isLoading,featuredSongs,error} =useMusicStore()
@@ -26,7 +27,7 @@ function FeaturedSection() {
                         <p className="font-medium truncate">{song.title}</p>
                         <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
                     </div>
-                    {/* TODO: add play button */}
+                    <PlayButton song={song} />
                 </div>
             ))
         }
--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,6 +1,7 @@
 import { Song } from "@/types";
 import SectionGridSkeleton from "./SectionGridSkeleton";
 import { Button } from "@/components/ui/button";
+import PlayButton from "./PlayButton";
 
 type SectionGridProps={
     title:string;
@@ -33,8 +34,9 @@ function SectionGrid({title,songs,isLoading}:SectionGridProps) {
                                     alt={song.title}
                                     className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                                 />
-                                {/* TODO: add play button */}
+                                
                             </div>
+                            <PlayButton song={song} />
                         </div>
                         <div>
                             <h3 className="font-medium mb-2 truncate">
